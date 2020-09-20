5bfd3c50dead12ce265d969cf541af10fa755a9d
dunglason6789p <dunglason6789p@gmail.com>
2020-09-20 11:34:44 +0700

    4th commit --good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: string;
+  name: string;
+  cost: number;
+  costOld: number;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'try-angular';
+  newProductListCarousel: Product[] = [
+    {
+      id: this.generateNewId() + '',
+      name: 'Bánh Hamburger',
+      cost: 123456,
+      costOld: 456789,
+      imageUrl: './pizzza/6.jpg'
+    }
+  ];
+
+  nextIdForGenerator = 0;
+  generateNewId(): number {
+    return this.nextIdForGenerator++;
+  }
 }
